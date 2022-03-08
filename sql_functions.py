9b87b29a8f6fd2642623d2ac093d35a82c32ebe6
Amir <amirlevdav@gmail.com>
2022-03-08 16:16:34 +0200

    mysql stuff

--- sql_functions.py
+++ sql_functions.py
@@ -1,6 +1,21 @@
 import mysql.connector
 from mysql.connector import Error
-import pandas as pd
+
+
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
 
 
 def create_server_connection(host_name, user_name, user_password):
@@ -29,22 +44,6 @@ def create_database(connection, query):
         print(f"Error: {e}")
 
 
-def create_db_connection(host_name, user_name, user_password, db_name):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=db_name
-        )
-        print("MySQL Database connection successful")
-    except Error as err:
-        print(f"Error: '{err}'")
-
-    return connection
-
-
 def execute_query(connection, query):
     cursor = connection.cursor()
     try:
@@ -55,4 +54,13 @@ def execute_query(connection, query):
         print(f"Error: {e}")
 
 
+def read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"Error: {e}")
 
